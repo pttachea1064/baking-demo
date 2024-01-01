daf3b6831173a17ad08d03683fc733aaa89ac8f0
pttachea1064 <pttachea1064@gmail.com>
2024-01-01 20:02:20 +0800

    基本準備

---
+++ src/main/java/practice/baking/BakingApplication.java
@@ -0,0 +1,13 @@
+package practice.baking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BakingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BakingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/practice/baking/BakingApplicationTests.java
@@ -0,0 +1,13 @@
+package practice.baking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BakingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
