5bda0b1aa659e83f994cb5ac97cbec30d0684223
pttachea1064 <pttachea1064@gmail.com>
2024-01-05 20:19:15 +0800

    權限內容

--- src/main/java/cn/tedu/baking/security/UserDetailsServiceImpl.java
+++ src/main/java/cn/tedu/baking/security/UserDetailsServiceImpl.java
@@ -3,6 +3,7 @@ package cn.tedu.baking.security;
 import cn.tedu.baking.mapper.UserMapper;
 import cn.tedu.baking.pojo.vo.UserVO;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.authority.AuthorityUtils;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -19,15 +20,25 @@ public class UserDetailsServiceImpl implements UserDetailsService {
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         UserVO userVO = userMapper.selectByUserName(username);
         if (userVO != null){
-            UserDetails userDetails = User.builder()
-                    .username(username)
-                    .password(userVO.getPassword())
-                    .disabled(false)
-                    .accountLocked(false)
-                    .accountExpired(false)
-                    .credentialsExpired(false)
-                    .authorities("權限名")
-                    .build();
+//            UserDetails userDetails = User.builder()
+//                    .username(username)
+//                    .password(userVO.getPassword())
+//                    .disabled(false)
+//                    .accountLocked(false)
+//                    .accountExpired(false)
+//                    .credentialsExpired(false)
+//                    .authorities("權限名")
+//                    .build();
+            CustomUserDetails userDetails = new CustomUserDetails(
+                    userVO.getId(),
+                    userVO.getNickName(),
+                    userVO.getIsAdmin(),
+                    userVO.getImgUrl(),
+                    username,
+                    userVO.getPassword(),
+                    AuthorityUtils.createAuthorityList("我的權限")
+            );
+
             return userDetails;
         }
 
