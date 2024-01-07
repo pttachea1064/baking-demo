3d2de7af932bdfea81f894071ec0b5fda115bb7c
pttachea1064 <pttachea1064@gmail.com>
2024-01-07 20:20:18 +0800

    Knife4j的添加配置設定

---
+++ src/main/java/cn/tedu/baking/config/Knife4jConfig.java
@@ -0,0 +1,34 @@
+package cn.tedu.baking.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2WebMvc;
+
+@Configuration
+@EnableSwagger2WebMvc
+public class Knife4jConfig {
+    //配置Swagger2的Docket的Bean实例
+    @Bean
+    public Docket createRestApi() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(apiInfo())
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("cn.tedu.baking.controller"))
+                .paths(PathSelectors.any())
+                .build();
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title("烘焙坊作品")
+                .description("烘焙坊作品線上API文件檔案")
+                .version("1.0")
+                .build();
+    }
+}
